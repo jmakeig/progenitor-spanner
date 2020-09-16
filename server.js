bf0cf12ad85c2ce14fd11598eadf7b77d71c1453
Justin Makeig <jmakeig@marklogic.com>
2020-09-16 12:14:02 -0700

    Adds initial Spanner connection

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const { Spanner } = require('@google-cloud/spanner');
+
+const spanner = new Spanner({
+  projectId: 'handy-limiter-230400',
+});
+const instance = spanner.instance('sandbox');
+const database = instance.database('progenitor');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+app.get('/', async (req, res) => {
+  // The query to execute
+  const query = {
+    sql: 'SELECT 1',
+  };
+
+  // Execute a simple SQL statement
+  const [rows] = await database.run(query);
+
+  res.send(`Rows: ${rows.join(', ')}`);
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
